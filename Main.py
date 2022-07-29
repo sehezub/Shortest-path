a84726794b6632d50b714e8bd9fdfeb2bbf6fae0
Sebastian Zubieta <sebastianzubieta04@gmail.com>
2022-07-28 21:29:14 -0300

    gui working

--- Main.py
+++ Main.py
@@ -1,10 +1,19 @@
 import pygame
 import math
+import time
 
-def draw_square_from_coordinate(screen, pos, n):
-    if pos[1] <= 25:
+def save_and_draw(screen, n, buffer, pos):
+    if buffer.count(pos) >= 1:
+        return buffer
+    else:
+        buffer.append(pos)
+        draw_square_from_coordinate(screen, pos, n, (255,255,255))
+        return buffer
+
+def draw_square_from_coordinate(screen, pos, n, color):
+    if pos[1] <= 24:
         return
-    pygame.draw.rect(screen, (255,255,255), (math.floor(pos[0]/n)*n, math.floor(pos[1]/n)*n, n, n))
+    pygame.draw.rect(screen, color, (math.floor(pos[0]/n)*n, 25+(math.floor((pos[1]-25)/n))*n, n, n))
 
 #just handling that the square size is ok
 try:
@@ -20,20 +29,46 @@ except (ValueError):
             break
         except(ValueError):
             continue
+#
+
 pygame.init()
 
-#n=10 #assert n\800,600
+states = list()
+buffer = list()
+is_hold_clicked = False
+
 screen = pygame.display.set_mode(size=(800,625))
-
 pygame.display.set_caption('Path finder')
-pygame.draw.line(screen, (255,0,0), (0,25), (800,25))
-#pygame.draw.rect(screen, (255,255,255), (0, 25, n, n))
 
-draw_square_from_coordinate(screen, (0,25), n)
+pygame.draw.line(screen, (255,0,0), (0,24), (800,24))
+pygame.draw.rect(screen, (0,255,0), (0, 0 , 40, 20))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            i = 0
+            for e in states:
+                i = i + len(e)
+            print(i)
             raise SystemExit
+
+        if is_hold_clicked:
+            buffer = save_and_draw(screen, n, buffer, pygame.mouse.get_pos())
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pos()[0] <= 40 and pygame.mouse.get_pos()[1] <= 20 and states != []:
+                for pos in states[-1]:
+                    draw_square_from_coordinate(screen, pos, n, (0,0,0))
+                del states[-1]
+            else:
+                is_hold_clicked = True
+                #print(pygame.mouse.get_pos())
+                buffer = save_and_draw(screen, n, buffer, pygame.mouse.get_pos())
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            is_hold_clicked = False
+            if buffer != []:
+                states.append(buffer.copy())
+                buffer.clear()
 
     pygame.display.update()
